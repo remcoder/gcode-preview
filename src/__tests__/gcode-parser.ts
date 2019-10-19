d02bf7b4f0e10fc853779b329186c9691e752bba
Remco Veldkamp <remcoder@gmail.com>
2019-10-19 20:54:47 +0200

    Add tests for the some elementry parsing cases

--- src/__tests__/gcode-parser.ts
+++ src/__tests__/gcode-parser.ts
@@ -1,12 +1,36 @@
 import { Parser } from "../gcode-parser";
 
-test('a single gcode cmd should result in 1 layer', () => {
+test('a single gcode cmd should result in 1 layer with 1 command', () => {
   const parser = new Parser();
   const gcode =`G1 X0 Y0 Z1`;
   const parsed = parser.parseGcode(gcode);
   expect(parsed).not.toBeNull();
   expect(parsed.layers).not.toBeNull();
   expect(parsed.layers.length).toEqual(1)
+  expect(parsed.layers[0].commands).not.toBeNull();
+  expect(parsed.layers[0].commands.length).toEqual(1);
+});
+
+test('2 horizontal moves should result in 1 layer with 2 commands', () => {
+  const parser = new Parser();
+  const gcode =`G1 X0 Y0 Z1
+  G1 X10 Y10 Z1`;
+  const parsed = parser.parseGcode(gcode);
+  expect(parsed).not.toBeNull();
+  expect(parsed.layers).not.toBeNull();
+  expect(parsed.layers.length).toEqual(1)
+  expect(parsed.layers[0].commands).not.toBeNull();
+  expect(parsed.layers[0].commands.length).toEqual(2);
+});
+
+test('2 vertical moves should result in 2 layers with 1 command', () => {
+  const parser = new Parser();
+  const gcode =`G1 X0 Y0 Z1
+  G1 X0 Y0 Z2`;
+  const parsed = parser.parseGcode(gcode);
+  expect(parsed).not.toBeNull();
+  expect(parsed.layers).not.toBeNull();
+  expect(parsed.layers.length).toEqual(2)
   expect(parsed.layers[0].commands).not.toBeNull();
   expect(parsed.layers[0].commands.length).toEqual(1);
 });
