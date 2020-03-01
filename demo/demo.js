1e466abed3b6157ace2e07998618df8f739f05a4
Remco Veldkamp <remcoder@gmail.com>
2020-03-01 16:05:01 +0100

    Cleanup syntax

--- demo/demo.js
+++ demo/demo.js
@@ -10,12 +10,12 @@ const fileSize = document.getElementById('file-size');
 const snapshot = document.getElementById('snapshot');
 
 function initDemo() {
-  const preview = window.preview  = new GCodePreview.WebGLPreview({
+  const preview = (window.preview = new GCodePreview.WebGLPreview({
     canvas: document.querySelector('.gcode-previewer'),
     // targetId : 'renderer',
     topLayerColor: new THREE.Color(`hsl(180, 50%, 50%)`).getHex(),
     lastSegmentColor: new THREE.Color(`hsl(270, 50%, 50%)`).getHex()
-  });
+  }));
 
   slider.addEventListener('input', function(evt) {
     preview.limit = +slider.value;
@@ -36,8 +36,7 @@ function initDemo() {
     if (toggleHighlight.checked) {
       preview.topLayerColor = new THREE.Color(`hsl(180, 50%, 50%)`).getHex();
       preview.lastSegmentColor = new THREE.Color(`hsl(270, 50%, 50%)`).getHex();
-    }
-    else {
+    } else {
       preview.topLayerColor = null;
       preview.lastSegmentColor = null;
     }
@@ -78,94 +77,94 @@ function initDemo() {
   });
 
   gcodePreview = preview;
-  
+
   updateUI();
 
   return preview;
 }
 
 function updateUI() {
-  slider.setAttribute('max', gcodePreview.layers.length-1);
-  slider.value = gcodePreview.layers.length-1;
-  layerCount.innerText = gcodePreview.layers && gcodePreview.layers.length + ' layers';
-  
-  console.log(gcodePreview.layers)
+  slider.setAttribute('max', gcodePreview.layers.length - 1);
+  slider.value = gcodePreview.layers.length - 1;
+  layerCount.innerText =
+    gcodePreview.layers && gcodePreview.layers.length + ' layers';
+
+  // console.log(gcodePreview.layers);
 
   if (gcodePreview.renderExtrusion)
-    toggleExtrusion.setAttribute("checked", "checked");
-  else
-    toggleExtrusion.removeAttribute("checked");
-  
+    toggleExtrusion.setAttribute('checked', 'checked');
+  else toggleExtrusion.removeAttribute('checked');
+
   if (gcodePreview.renderTravel)
-    toggleTravel.setAttribute("checked", "checked");
-  else
-    toggleTravel.removeAttribute("checked");
+    toggleTravel.setAttribute('checked', 'checked');
+  else toggleTravel.removeAttribute('checked');
 
   if (gcodePreview.topLayerColor !== undefined)
-    toggleHighlight.setAttribute("checked", "checked");
-  else
-    toggleHighlight.removeAttribute("checked");
+    toggleHighlight.setAttribute('checked', 'checked');
+  else toggleHighlight.removeAttribute('checked');
 }
 
 function loadGCode(file) {
   const reader = new FileReader();
   reader.onload = function() {
     _handleGCode(file.name, reader.result);
-  }
+  };
   reader.readAsText(file);
   fileName.setAttribute('href', '#');
 }
 
 async function loadGCodeFromServer(file) {
   const response = await fetch(file);
-    
+
   if (response.status !== 200) {
-    console.error('ERROR. Status Code: ' +
-      response.status);
+    console.error('ERROR. Status Code: ' + response.status);
     return;
   }
 
-  const gcode = await response.text()
-  _handleGCode(file, gcode); 
+  const gcode = await response.text();
+  _handleGCode(file, gcode);
   fileName.setAttribute('href', file);
 }
 
 function _handleGCode(filename, gcode) {
   fileName.innerText = filename;
   fileSize.innerText = humanFileSize(gcode.length);
-  
-  updateUI();  
-  
+
+  updateUI();
+
   startLoadingProgressive(gcode);
 }
 
 function startLoadingProgressive(gcode) {
   let c = 0;
   function loadProgressive() {
-    const start = c*chunkSize;
-    const end = (c+1)*chunkSize;
+    const start = c * chunkSize;
+    const end = (c + 1) * chunkSize;
     const chunk = lines.slice(start, end);
     gcodePreview.processGCode(chunk);
     updateUI();
     c++;
-    if (c < chunks) { 
-      window.__loadTimer__ = setTimeout(loadProgressive, 100);
+    if (c < chunks) {
+      window.__loadTimer__ = setTimeout(loadProgressive, 25);
     }
   }
   const lines = gcode.split('\n');
   console.log('lines', lines.length);
-  const chunkSize = 1000;
+  const chunkSize = 100;
   console.log('chunk size', chunkSize);
   const chunks = lines.length / chunkSize;
   console.log('chunks', chunks);
-  console.log('loading')
+  console.log('loading');
   gcodePreview.clear();
-  if (window.__loadTimer__) 
-      clearTimeout(window.__loadTimer__);
+  if (window.__loadTimer__) clearTimeout(window.__loadTimer__);
   loadProgressive();
 }
 
 function humanFileSize(size) {
-  var i = Math.floor( Math.log(size) / Math.log(1024) );
-  return ( size / Math.pow(1024, i) ).toFixed(2) * 1 + ' ' + ['B', 'kB', 'MB', 'GB', 'TB'][i];
-};
+  var i = Math.floor(Math.log(size) / Math.log(1024));
+  return (
+    (size / Math.pow(1024, i)).toFixed(2) * 1 +
+    ' ' +
+    ['B', 'kB', 'MB', 'GB', 'TB'][i]
+  );
+}
--- src/webgl-preview.ts
+++ src/webgl-preview.ts
@@ -1,57 +1,58 @@
 import { Parser, MoveCommand } from './gcode-parser';
 import * as THREE from 'three';
-import * as OrbitControls  from 'three-orbitcontrols';
+import * as OrbitControls from 'three-orbitcontrols';
 import { LineMaterial } from './three-line2/LineMaterial';
 import { LineGeometry } from './three-line2/LineGeometry';
 import { LineSegments2 } from './three-line2/LineSegments2';
 
-type RenderLayer = { extrusion: number[], travel: number[], z: number };
-type Point = {x:number, y:number, z:number};
-type State = {x:number, y:number, z:number, e:number}; // feedrate?
+type RenderLayer = { extrusion: number[]; travel: number[]; z: number };
+type Point = { x: number; y: number; z: number };
+type State = { x: number; y: number; z: number; e: number }; // feedrate?
 
 type WebGLPreviewOptions = {
-  canvas?: HTMLCanvasElement,
-  targetId: string,
-  limit?: number,
-  topLayerColor?: number,
-  lastSegmentColor?: number,
-  lineWidth: number
-}
+  canvas?: HTMLCanvasElement;
+  targetId: string;
+  limit?: number;
+  topLayerColor?: number;
+  lastSegmentColor?: number;
+  lineWidth: number;
+};
 
 export class WebGLPreview {
-  parser = new Parser()
-  limit?: number
-  targetId: string
-  scene: THREE.Scene
-  camera: THREE.PerspectiveCamera
-  renderer: THREE.WebGLRenderer
-  group: THREE.Group
-  backgroundColor = 0xe0e0e0
-  travelColor = 0x990000
-  extrusionColor = 0x00FF00
-  topLayerColor? : number
-  lastSegmentColor? : number
-  container: HTMLElement
-  canvas : HTMLCanvasElement
-  renderExtrusion = true
-  renderTravel = false
-  lineWidth : number | null = null
+  parser = new Parser();
+  limit?: number;
+  targetId: string;
+  scene: THREE.Scene;
+  camera: THREE.PerspectiveCamera;
+  renderer: THREE.WebGLRenderer;
+  group: THREE.Group;
+  backgroundColor = 0xe0e0e0;
+  travelColor = 0x990000;
+  extrusionColor = 0x00ff00;
+  topLayerColor?: number;
+  lastSegmentColor?: number;
+  container: HTMLElement;
+  canvas: HTMLCanvasElement;
+  renderExtrusion = true;
+  renderTravel = false;
+  lineWidth: number | null = null;
 
   constructor(opts: WebGLPreviewOptions) {
-    this.scene =  new THREE.Scene();
-    this.scene.background = new THREE.Color( this.backgroundColor );
+    this.scene = new THREE.Scene();
+    this.scene.background = new THREE.Color(this.backgroundColor);
     this.canvas = opts.canvas;
     this.targetId = opts.targetId;
     this.limit = opts.limit;
     this.topLayerColor = opts.topLayerColor;
     this.lastSegmentColor = opts.lastSegmentColor;
     this.lineWidth = opts.lineWidth;
-    
+
     console.debug('opts', opts);
 
     if (!this.canvas) {
       const container = document.getElementById(this.targetId);
-      if (!container) throw new Error('Unable to find element ' + this.targetId);
+      if (!container)
+        throw new Error('Unable to find element ' + this.targetId);
 
       this.renderer = new THREE.WebGLRenderer({preserveDrawingBuffer: true});
       this.canvas = this.renderer.domElement;
@@ -79,8 +80,8 @@ export class WebGLPreview {
   }
 
   animate() {
-    requestAnimationFrame(() => this.animate() );
-    this.renderer.render( this.scene, this.camera );
+    requestAnimationFrame(() => this.animate());
+    this.renderer.render(this.scene, this.camera);
   }
 
   processGCode(gcode: string | string[]) {
@@ -89,70 +90,82 @@ export class WebGLPreview {
   }
 
   render() {
-    while(this.scene.children.length > 0){ 
-      this.scene.remove(this.scene.children[0]); 
+    while (this.scene.children.length > 0) {
+      this.scene.remove(this.scene.children[0]);
     }
     this.group = new THREE.Group();
     this.group.name = 'gcode';
-    const state = {x:0, y:0, z:0, e:0};
+    const state = { x: 0, y: 0, z: 0, e: 0 };
 
-    for (let index=0 ; index < this.layers.length ; index++ ) {
+    for (let index = 0; index < this.layers.length; index++) {
       if (index > this.limit) break;
-    
-      const currentLayer : RenderLayer = { extrusion: [], travel: [], z: state.z };
+
+      const currentLayer: RenderLayer = {
+        extrusion: [],
+        travel: [],
+        z: state.z
+      };
       const l = this.layers[index];
       for (const cmd of l.commands) {
         if (cmd.gcode == 'g0' || cmd.gcode == 'g1') {
-          const g = (cmd as MoveCommand);
-          
-          const next : State = {
+          const g = cmd as MoveCommand;
+
+          const next: State = {
             x: g.params.x !== undefined ? g.params.x : state.x,
             y: g.params.y !== undefined ? g.params.y : state.y,
             z: g.params.z !== undefined ? g.params.z : state.z,
             e: g.params.e !== undefined ? g.params.e : state.e
           };
           const extrude = g.params.e > 0;
-          if (extrude && this.renderExtrusion || !extrude && this.renderTravel)
+          if (
+            (extrude && this.renderExtrusion) ||
+            (!extrude && this.renderTravel)
+          )
             this.addLineSegment(currentLayer, state, next, extrude);
-          
-          // update state 
+
+          // update state
           if (g.params.x) state.x = g.params.x;
           if (g.params.y) state.y = g.params.y;
           if (g.params.z) state.z = g.params.z;
           if (g.params.e) state.e = g.params.e;
         }
       }
-      
+
       if (this.renderExtrusion) {
-        const brightness = Math.round(80 * index/this.layers.length);
-        const extrusionColor = new THREE.Color(`hsl(0, 0%, ${brightness}%)`).getHex();
-        
-        if(index == this.layers.length - 1) {
-          const layerColor = this.topLayerColor !== undefined ? 
-            this.topLayerColor :
-            extrusionColor;
-          
-          const lastSegmentColor = this.lastSegmentColor !== undefined ? this.lastSegmentColor : layerColor;
+        const brightness = Math.round((80 * index) / this.layers.length);
+        const extrusionColor = new THREE.Color(
+          `hsl(0, 0%, ${brightness}%)`
+        ).getHex();
+
+        if (index == this.layers.length - 1) {
+          const layerColor =
+            this.topLayerColor !== undefined
+              ? this.topLayerColor
+              : extrusionColor;
+
+          const lastSegmentColor =
+            this.lastSegmentColor !== undefined
+              ? this.lastSegmentColor
+              : layerColor;
 
           const endPoint = currentLayer.extrusion.splice(-3);
-          this.addLine( currentLayer.extrusion, layerColor);
+          this.addLine(currentLayer.extrusion, layerColor);
           const preendPoint = currentLayer.extrusion.splice(-3);
-          this.addLine( [...preendPoint, ...endPoint], lastSegmentColor);
-        }
-        else {
-          this.addLine( currentLayer.extrusion, extrusionColor);
+          this.addLine([...preendPoint, ...endPoint], lastSegmentColor);
+        } else {
+          this.addLine(currentLayer.extrusion, extrusionColor);
         }
       }
-      
+
       if (this.renderTravel) {
-        this.addLine( currentLayer.travel, this.travelColor);
+        this.addLine(currentLayer.travel, this.travelColor);
       }
     }
 
-    this.group.quaternion.setFromEuler( new THREE.Euler( -Math.PI/2, 0, 0 ) );
-    this.group.position.set( - 100, - 20, 100 );
-    this.scene.add( this.group );
-    this.renderer.render( this.scene, this.camera );
+    this.group.quaternion.setFromEuler(new THREE.Euler(-Math.PI / 2, 0, 0));
+    this.group.position.set(-100, -20, 100);
+    this.scene.add(this.group);
+    this.renderer.render(this.scene, this.camera);
   }
 
   clear() {
@@ -170,33 +183,36 @@ export class WebGLPreview {
 
   addLineSegment(layer: RenderLayer, p1: Point, p2: Point, extrude: boolean) {
     const line = extrude ? layer.extrusion : layer.travel;
-    line.push( p1.x, p1.y, p1.z, p2.x, p2.y, p2.z );
+    line.push(p1.x, p1.y, p1.z, p2.x, p2.y, p2.z);
   }
 
   addLine(vertices: number[], color: number) {
-    if ( typeof this.lineWidth == "number" ) {
-      this.addThickLine( vertices, color);
+    if (typeof this.lineWidth == 'number') {
+      this.addThickLine(vertices, color);
       return;
     }
-    
+
     const geometry = new THREE.BufferGeometry();
-    geometry.setAttribute( 'position', new THREE.Float32BufferAttribute( vertices, 3 ) );
-    
-    const material = new THREE.LineBasicMaterial( { color: color } );
-    const lineSegments = new THREE.LineSegments( geometry, material );
-    this.group.add( lineSegments );
+    geometry.setAttribute(
+      'position',
+      new THREE.Float32BufferAttribute(vertices, 3)
+    );
+
+    const material = new THREE.LineBasicMaterial({ color: color });
+    const lineSegments = new THREE.LineSegments(geometry, material);
+    this.group.add(lineSegments);
   }
 
   addThickLine(vertices: number[], color: number) {
     const geometry = new LineGeometry();
-    geometry.setPositions( vertices );
+    geometry.setPositions(vertices);
 
     const matLine = new LineMaterial({
       color: color,
-      linewidth: this.lineWidth, // in pixels
+      linewidth: this.lineWidth // in pixels
     });
 
-    const line = new LineSegments2( geometry, matLine );
+    const line = new LineSegments2(geometry, matLine);
     // line.computeLineDistances();
     // line.scale.set( .1, .1, .1 );
     this.group.add(line);
