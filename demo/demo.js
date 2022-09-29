82a470c371ff0713e651d8d3043ad5ce673a6a48
Remco Veldkamp <remcoder@gmail.com>
2022-09-29 22:34:50 +0200

    Expose controls

--- demo/demo.js
+++ demo/demo.js
@@ -41,6 +41,7 @@ function initDemo() { // eslint-disable-line no-unused-vars, @typescript-eslint/
 
   preview.renderExtrusion = true;
   preview.renderTravel = false;
+  // preview.controls.autoRotate = true;
 
   startLayer.addEventListener('input', function() {
     preview.startLayer = +startLayer.value;
