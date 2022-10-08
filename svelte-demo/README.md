1717419113e8fc12e7d510fe90a1599285ae46dc
Remco Veldkamp <remcoder@gmail.com>
2022-10-08 21:45:46 +0200

    Add svelte demo

---
+++ svelte-demo/README.md
@@ -0,0 +1,12 @@
+# GCodePreview with Svelte + Vite
+
+## Svelte component
+
+Using the example Svelte component is a simple as:
+```
+<script>
+  import GCodePreview from './lib/GCodePreview.svelte';
+</script>
+
+<GCodePreview src={'benchy.gcode'} />
+```
---
+++ svelte-demo/src/lib/GCodePreview.svelte
@@ -0,0 +1,79 @@
+<script lang="ts">
+import * as GCodePreview from 'gcode-preview';
+import { onMount } from 'svelte';
+
+export let src;
+export let chunkSize = Infinity;
+
+let canvas;
+let preview;
+let __animationTimer__;
+
+$: {
+    load(src);
+}
+
+onMount(() => {
+    window['preview'] = preview = GCodePreview.init({
+        canvas,
+        allowDragNDrop: true  
+    });
+
+    load(src);
+});
+
+async function load(src) {
+    preview && loadChuncked(preview, await fetchGcode(src),50);
+}
+
+async function fetchGcode(url) {
+    const response = await fetch(url);
+
+    if (response.status !== 200) {
+        throw new Error(`status code: ${response.status}`);
+    }
+
+    const file = await response.text();
+    return file.split('\n');
+}
+
+function loadChuncked(preview,lines,delay) {
+    let c = 0;
+    
+    preview.clear();
+    if (__animationTimer__)
+        window.clearTimeout(__animationTimer__);
+
+    const loadProgressive = () => {
+        const _chunkSize = chunkSize ?? Infinity;
+        const start = c * _chunkSize;
+        const end = (c + 1) * _chunkSize;
+        const chunk = lines.slice(start, end);
+
+        preview.processGCode(chunk);
+        c++;
+        if (c * _chunkSize < lines.length) {
+            __animationTimer__ = setTimeout(loadProgressive, delay);
+        }
+    };
+
+    // cancel loading process if one is still in progress
+    // mostly when hot reloading
+    window.clearTimeout(__animationTimer__);
+    loadProgressive();
+};
+
+</script>
+
+<canvas
+    bind:this={canvas}
+    width={300}
+    height={200}>
+</canvas>
+
+
+<style>
+    canvas {
+        cursor: grab;
+    }
+</style>
