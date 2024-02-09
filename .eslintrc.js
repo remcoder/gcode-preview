1a2d2ec7b0cf8fabc848aebe29e36a987cf78f65
Remco Veldkamp <remcoder@gmail.com>
2024-02-09 11:59:05 +0100

    Group examples

--- .eslintrc.js
+++ .eslintrc.js
@@ -4,7 +4,7 @@ module.exports = {
   parser: '@typescript-eslint/parser',
   plugins: ['@typescript-eslint'],
   extends: ['eslint:recommended', 'plugin:@typescript-eslint/recommended'],
-  ignorePatterns: ['dist'],
+  ignorePatterns: ['dist', 'examples'],
   env: {
     browser: true
   }
