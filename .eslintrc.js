14d01db6f4bcf241bc49c7627326b3d01fc5d8c2
Remco Veldkamp <remcoder@gmail.com>
2024-02-16 22:36:51 +0100

    Tweak linting

--- .eslintrc.js
+++ .eslintrc.js
@@ -4,6 +4,11 @@ module.exports = {
   parser: '@typescript-eslint/parser',
   plugins: ['@typescript-eslint'],
   extends: ['eslint:recommended', 'plugin:@typescript-eslint/recommended'],
+  rules: {
+    'no-unused-vars': 'off',
+    '@typescript-eslint/no-unused-vars': ['error'],
+    '@typescript-eslint/no-unsafe-declaration-merging': 'off'
+  },
   ignorePatterns: ['dist', 'examples'],
   env: {
     browser: true
