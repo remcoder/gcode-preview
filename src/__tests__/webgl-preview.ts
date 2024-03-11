efb91f8f91be95b82c64390e892e3937e2491adc
Remco Veldkamp <remcoder@gmail.com>
2024-03-11 16:26:30 +0100

    Update tests with changed state handling

--- src/__tests__/webgl-preview.ts
+++ src/__tests__/webgl-preview.ts
@@ -6,29 +6,29 @@ import { State, WebGLPreview } from '../webgl-preview';
 import { GCodeCommand } from '../gcode-parser';
 
 test('in gcode x,y,z params should update the state', () => {
-  const state: State = { x: 0, y: 0, z: 0, r: 0, e: 0, i: 0, j: 0 };
   const mock = createMockPreview();
   mock.layers[0].commands.push(new GCodeCommand('', 'g0', { x: 1, y: 1, z: 1, e: 1 }));
   const layerIndex = 0;
-  WebGLPreview.prototype.renderLayer.call(mock, layerIndex, state);
-  expect(state.x).toBe(1);
-  expect(state.y).toBe(1);
-  expect(state.z).toBe(1);
+  WebGLPreview.prototype.renderLayer.call(mock, layerIndex);
+  expect(mock.state.x).toBe(1);
+  expect(mock.state.y).toBe(1);
+  expect(mock.state.z).toBe(1);
 });
 
 test('x,y,z params can go to 0', () => {
-  const state: State = { x: 1, y: 1, z: 1, r: 0, e: 1, i: 0, j: 0 };
   const mock = createMockPreview();
   mock.layers[0].commands.push(new GCodeCommand('', 'g0', { x: 0, y: 0, z: 0, e: 0 }));
   const layerIndex = 0;
-  WebGLPreview.prototype.renderLayer.call(mock, layerIndex, state);
-  expect(state.x).toBe(0);
-  expect(state.y).toBe(0);
-  expect(state.z).toBe(0);
+  WebGLPreview.prototype.renderLayer.call(mock, layerIndex);
+  expect(mock.state.x).toBe(0);
+  expect(mock.state.y).toBe(0);
+  expect(mock.state.z).toBe(0);
 });
 
 function createMockPreview() {
   return {
+    state: { x: 0, y: 0, z: 0, r: 0, e: 0, i: 0, j: 0 },
+
     minLayerIndex: 0,
     maxLayerIndex: Infinity,
     layers: [
