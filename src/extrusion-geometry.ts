b3de09aedb3e74f794b845dd5514d5baada1723b
Sophie Déziel <courrier@sophiedeziel.com>
2024-06-02 19:31:01 -0400

    Module imports for demo

--- src/extrusion-geometry.ts
+++ src/extrusion-geometry.ts
@@ -8,6 +8,7 @@ class ExtrusionGeometry extends BufferGeometry {
     radialSegments: number;
     closed: boolean;
   };
+  readonly type: 'ExtrusionGeometry';
   constructor(
     points: Vector3[] = [new Vector3()],
     lineWidth: number = 0.6,
@@ -15,8 +16,6 @@ class ExtrusionGeometry extends BufferGeometry {
     radialSegments: number = 8
   ) {
     super();
-
-    this.type = 'ExtrusionGeometry';
 
     this.parameters = {
       points: points,
