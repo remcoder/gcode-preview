45a6d1d3626a5d105fab95e340e994ad02971f69
Remco Veldkamp <remcoder@gmail.com>
2024-06-29 16:48:06 +0200

    Initial working version

---
+++ svelte-demo/src/routes/+page.svelte
@@ -0,0 +1,24 @@
+<script lang="ts">
+	import { onMount } from 'svelte';
+	import * as GCodePreview from 'gcode-preview';
+
+	let canvas: HTMLCanvasElement;
+	let preview: GCodePreview.WebGLPreview;
+
+	onMount(() => {
+		let preview = GCodePreview.init({
+			canvas,
+			devMode: false,
+			buildVolume: {
+				x: 200,
+				y: 200,
+				z: 0
+			}
+		});
+
+		preview.processGCode('G0 X0 Y0 Z0\nG1 X10 Y10 Z10 E10');
+	});
+</script>
+
+<h1>GCode Preview</h1>
+<canvas bind:this={canvas}></canvas>
