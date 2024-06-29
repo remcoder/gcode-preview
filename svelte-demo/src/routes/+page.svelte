93db4974c269d60db1e310fab717c16fbcd4496e
Remco Veldkamp <remcoder@gmail.com>
2024-06-29 21:51:48 +0200

    Added flowbite

--- svelte-demo/src/routes/+page.svelte
+++ svelte-demo/src/routes/+page.svelte
@@ -1,6 +1,13 @@
 <script lang="ts">
 	import GCode from '../lib/gcode.svelte';
-</script>
+
+  import { Alert } from 'flowbite-svelte';</script>
 
 <h1>GCode Preview</h1>
+<div class="p-8">
+  <Alert>
+    <span class="font-medium">Info alert!</span>
+    Change a few things up and try submitting again.
+  </Alert>
+</div>
 <GCode src="benchy.gcode" />
--- svelte-demo/tailwind.config.ts
+++ svelte-demo/tailwind.config.ts
@@ -1,11 +1,29 @@
+import flowbitePlugin from 'flowbite/plugin'
+
 import type { Config } from 'tailwindcss';
 
 export default {
-	content: ['./src/**/*.{html,js,svelte,ts}'],
+	content: ['./src/**/*.{html,js,svelte,ts}', './node_modules/flowbite-svelte/**/*.{html,js,svelte,ts}'],
 
 	theme: {
-		extend: {}
+		extend: {
+      colors: {
+        // flowbite-svelte
+        primary: {
+          50: '#FFF5F2',
+          100: '#FFF1EE',
+          200: '#FFE4DE',
+          300: '#FFD5CC',
+          400: '#FFBCAD',
+          500: '#FE795D',
+          600: '#EF562F',
+          700: '#EB4F27',
+          800: '#CC4522',
+          900: '#A5371B'
+        }
+      }
+    }
 	},
 
-	plugins: [require('@tailwindcss/typography')]
+	plugins: [flowbitePlugin]
 } as Config;
