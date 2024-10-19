403827ece2779aa89d35e2544079358cbc00d801
Sophie Déziel <courrier@sophiedeziel.com>
2024-10-18 22:15:40 -0400

    Bring back a representation of layers (#233)
    * Remove the code enum (#229)
    
    * Bring back a representation of layers
    
    * No need to expose isPlanar now
    
    * Make layers 100% progressive rendering ready
    
    * Layer number, height and z
    
    * fix the splitting logic

--- src/__tests__/interpreter.ts
+++ src/__tests__/interpreter.ts
@@ -1,200 +1,202 @@
-import { test, expect } from 'vitest';
+import { test, expect, describe } from 'vitest';
 import { GCodeCommand } from '../gcode-parser';
 import { Interpreter } from '../interpreter';
 import { Job } from '../job';
 import { PathType } from '../path';
 
-test('.execute returns a stateful job', () => {
-  const command = new GCodeCommand('G0 X1 Y2 Z3', 'g0', { x: 1, y: 2, z: 3 });
-  const interpreter = new Interpreter();
+describe('.execute', () => {
+  test('returns a stateful job', () => {
+    const command = new GCodeCommand('G0 X1 Y2 Z3', 'g0', { x: 1, y: 2, z: 3 });
+    const interpreter = new Interpreter();
 
-  const result = interpreter.execute([command]);
+    const result = interpreter.execute([command]);
 
-  expect(result).not.toBeNull();
-  expect(result).toBeInstanceOf(Job);
-  expect(result.state.x).toEqual(1);
-  expect(result.state.y).toEqual(2);
-  expect(result.state.z).toEqual(3);
+    expect(result).not.toBeNull();
+    expect(result).toBeInstanceOf(Job);
+    expect(result.state.x).toEqual(1);
+    expect(result.state.y).toEqual(2);
+    expect(result.state.z).toEqual(3);
+  });
+
+  test('ignores unknown commands', () => {
+    const command = new GCodeCommand('G42', 'g42', {});
+    const interpreter = new Interpreter();
+
+    const result = interpreter.execute([command]);
+
+    expect(result).not.toBeNull();
+    expect(result).toBeInstanceOf(Job);
+    expect(result.state.x).toEqual(0);
+    expect(result.state.y).toEqual(0);
+    expect(result.state.z).toEqual(0);
+  });
+
+  test('runs multiple commands', () => {
+    const command1 = new GCodeCommand('G0 X1 Y2 Z3', 'g0', { x: 1, y: 2, z: 3 });
+    const command2 = new GCodeCommand('G0 X4 Y5 Z6', 'g0', { x: 4, y: 5, z: 6 });
+    const interpreter = new Interpreter();
+
+    const result = interpreter.execute([command1, command2, command1, command2]);
+
+    expect(result).not.toBeNull();
+    expect(result).toBeInstanceOf(Job);
+    expect(result.state.x).toEqual(4);
+    expect(result.state.y).toEqual(5);
+    expect(result.state.z).toEqual(6);
+  });
+
+  test('runs on an existing job', () => {
+    const job = new Job();
+    const command = new GCodeCommand('G0 X1 Y2 Z3', 'g0', { x: 1, y: 2, z: 3 });
+    const interpreter = new Interpreter();
+
+    const result = interpreter.execute([command], job);
+
+    expect(result).toEqual(job);
+    expect(result.state.x).toEqual(1);
+    expect(result.state.y).toEqual(2);
+    expect(result.state.z).toEqual(3);
+  });
+
+  test('finishes the current path at the end of the job', () => {
+    const job = new Job();
+    const command = new GCodeCommand('G0 X1 Y2 Z3', 'g0', { x: 1, y: 2, z: 3 });
+    const interpreter = new Interpreter();
+    interpreter.execute([command], job);
+
+    expect(job.paths.length).toEqual(1);
+    expect(job.inprogressPath).toBeUndefined();
+  });
+
+  test('resumes the current path when doing incremental execution', () => {
+    const job = new Job();
+    const command1 = new GCodeCommand('G0 X1 Y2 Z3', 'g0', { x: 1, y: 2, z: 3 });
+    const command2 = new GCodeCommand('G0 X4 Y5 Z6', 'g0', { x: 4, y: 5, z: 6 });
+    const interpreter = new Interpreter();
+
+    interpreter.execute([command1], job);
+    interpreter.execute([command2], job);
+
+    expect(job.paths.length).toEqual(1);
+    expect(job.paths[0].vertices.length).toEqual(9);
+    expect(job.paths[0].vertices[6]).toEqual(command2.params.x);
+    expect(job.paths[0].vertices[7]).toEqual(command2.params.y);
+    expect(job.paths[0].vertices[8]).toEqual(command2.params.z);
+  });
 });
 
-test('.execute ignores unknown commands', () => {
-  const command = new GCodeCommand('G42', 'g42', {});
-  const interpreter = new Interpreter();
+describe('.g0', () => {
+  test('starts a path if the job has none, starting at the job current state', () => {
+    const command = new GCodeCommand('G0 X1 Y2', 'g0', { x: 1, y: 2 });
+    const interpreter = new Interpreter();
+    const job = new Job();
+    job.state.x = 3;
+    job.state.y = 4;
+    job.state.tool = 5;
 
-  const result = interpreter.execute([command]);
+    interpreter.g0(command, job);
 
-  expect(result).not.toBeNull();
-  expect(result).toBeInstanceOf(Job);
-  expect(result.state.x).toEqual(0);
-  expect(result.state.y).toEqual(0);
-  expect(result.state.z).toEqual(0);
-});
+    expect(job.paths.length).toEqual(0);
+    expect(job.inprogressPath?.vertices.length).toEqual(6);
+    expect(job.inprogressPath?.vertices[0]).toEqual(3);
+    expect(job.inprogressPath?.vertices[1]).toEqual(4);
+    expect(job.inprogressPath?.vertices[2]).toEqual(0);
+    expect(job.inprogressPath?.tool).toEqual(5);
+  });
 
-test('.execute runs multiple commands', () => {
-  const command1 = new GCodeCommand('G0 X1 Y2 Z3', 'g0', { x: 1, y: 2, z: 3 });
-  const command2 = new GCodeCommand('G0 X4 Y5 Z6', 'g0', { x: 4, y: 5, z: 6 });
-  const interpreter = new Interpreter();
+  test('continues the path if the job has one', () => {
+    const command1 = new GCodeCommand('G0 X1 Y2', 'g0', { x: 1, y: 2 });
+    const command2 = new GCodeCommand('G0 X3 Y4', 'g0', { x: 3, y: 4 });
+    const interpreter = new Interpreter();
+    const job = new Job();
 
-  const result = interpreter.execute([command1, command2, command1, command2]);
+    job.state.z = 5;
+    interpreter.g0(command1, job);
 
-  expect(result).not.toBeNull();
-  expect(result).toBeInstanceOf(Job);
-  expect(result.state.x).toEqual(4);
-  expect(result.state.y).toEqual(5);
-  expect(result.state.z).toEqual(6);
-});
+    interpreter.g0(command2, job);
 
-test('.execute runs on an existing job', () => {
-  const job = new Job();
-  const command = new GCodeCommand('G0 X1 Y2 Z3', 'g0', { x: 1, y: 2, z: 3 });
-  const interpreter = new Interpreter();
+    expect(job.paths.length).toEqual(0);
+    expect(job.inprogressPath?.vertices.length).toEqual(9);
+    expect(job.inprogressPath?.vertices[6]).toEqual(command2.params.x);
+    expect(job.inprogressPath?.vertices[7]).toEqual(command2.params.y);
+    expect(job.inprogressPath?.vertices[8]).toEqual(job.state.z);
+  });
 
-  const result = interpreter.execute([command], job);
+  test("assigns the travel type if there's no extrusion", () => {
+    const command = new GCodeCommand('G0 X1 Y2', 'g0', { x: 1, y: 2 });
+    const interpreter = new Interpreter();
+    const job = new Job();
 
-  expect(result).toEqual(job);
-  expect(result.state.x).toEqual(1);
-  expect(result.state.y).toEqual(2);
-  expect(result.state.z).toEqual(3);
-});
+    interpreter.g0(command, job);
 
-test('.G0 moves the state to the new position', () => {
-  const command = new GCodeCommand('G0 X1 Y2 Z3', 'g0', { x: 1, y: 2, z: 3 });
-  const interpreter = new Interpreter();
+    expect(job.paths.length).toEqual(0);
+    expect(job.inprogressPath?.travelType).toEqual(PathType.Travel);
+  });
 
-  const job = interpreter.execute([command]);
+  test("assigns the extrusion type if there's extrusion", () => {
+    const command = new GCodeCommand('G1 X1 Y2 E3', 'g1', { x: 1, y: 2, e: 3 });
+    const interpreter = new Interpreter();
+    const job = new Job();
 
-  expect(job.state.x).toEqual(1);
-  expect(job.state.y).toEqual(2);
-  expect(job.state.z).toEqual(3);
-});
+    interpreter.g0(command, job);
 
-test('.G0 starts a path if the job has none', () => {
-  const command = new GCodeCommand('G0 X1 Y2', 'g0', { x: 1, y: 2 });
-  const interpreter = new Interpreter();
+    expect(job.paths.length).toEqual(0);
+    expect(job.inprogressPath?.travelType).toEqual('Extrusion');
+  });
 
-  const job = interpreter.execute([command]);
+  test('assigns the travel type if the extrusion is a retraction', () => {
+    const command = new GCodeCommand('G0 E-2', 'g0', { e: -2 });
+    const interpreter = new Interpreter();
+    const job = new Job();
 
-  expect(job.paths.length).toEqual(0);
-  expect(job.inprogressPath).not.toBeNull();
-  expect(job.inprogressPath?.vertices.length).toEqual(6);
-  expect(job.inprogressPath?.vertices[0]).toEqual(0);
-  expect(job.inprogressPath?.vertices[1]).toEqual(0);
-  expect(job.inprogressPath?.vertices[2]).toEqual(0);
-  expect(job.inprogressPath?.vertices[3]).toEqual(1);
-  expect(job.inprogressPath?.vertices[4]).toEqual(2);
-  expect(job.inprogressPath?.vertices[5]).toEqual(0);
-});
+    interpreter.g0(command, job);
 
-test('.G0 starts a path if the job has none, starting at the job current state', () => {
-  const command = new GCodeCommand('G0 X1 Y2', 'g0', { x: 1, y: 2 });
-  const interpreter = new Interpreter();
-  const job = new Job();
-  job.state.x = 3;
-  job.state.y = 4;
-  job.state.tool = 5;
+    expect(job.paths.length).toEqual(0);
+    expect(job.inprogressPath?.travelType).toEqual('Travel');
+  });
 
-  interpreter.execute([command], job);
+  test('assigns the travel type if the extrusion is a retraction', () => {
+    const command = new GCodeCommand('G0 E-2', 'g0', { e: -2 });
+    const interpreter = new Interpreter();
+    const job = new Job();
 
-  expect(job.paths.length).toEqual(0);
-  expect(job.inprogressPath?.vertices.length).toEqual(6);
-  expect(job.inprogressPath?.vertices[0]).toEqual(3);
-  expect(job.inprogressPath?.vertices[1]).toEqual(4);
-  expect(job.inprogressPath?.vertices[2]).toEqual(0);
-  expect(job.inprogressPath?.tool).toEqual(5);
-});
+    interpreter.g0(command, job);
 
-test('.G0 continues the path if the job has one', () => {
-  const command1 = new GCodeCommand('G0 X1 Y2', 'g0', { x: 1, y: 2 });
-  const command2 = new GCodeCommand('G0 X3 Y4', 'g0', { x: 3, y: 4 });
-  const interpreter = new Interpreter();
-  const job = new Job();
+    expect(job.paths.length).toEqual(0);
+    expect(job.inprogressPath?.travelType).toEqual('Travel');
+  });
 
-  job.state.z = 5;
-  interpreter.execute([command1], job);
+  test('starts a new path if the travel type changes from Travel to Extrusion', () => {
+    const command1 = new GCodeCommand('G0 X1 Y2', 'g0', { x: 1, y: 2 });
+    const command2 = new GCodeCommand('G1 X3 Y4 E5', 'g1', { x: 3, y: 4, e: 5 });
+    const interpreter = new Interpreter();
+    const job = new Job();
+    interpreter.execute([command1], job);
 
-  interpreter.g0(command2, job);
+    interpreter.g0(command2, job);
 
-  expect(job.paths.length).toEqual(0);
-  expect(job.inprogressPath?.vertices.length).toEqual(9);
-  expect(job.inprogressPath?.vertices[6]).toEqual(3);
-  expect(job.inprogressPath?.vertices[7]).toEqual(4);
-  expect(job.inprogressPath?.vertices[8]).toEqual(5);
-});
+    expect(job.paths.length).toEqual(1);
+    expect(job.inprogressPath?.travelType).toEqual('Extrusion');
+  });
 
-test(".G0 assigns the travel type if there's no extrusion", () => {
-  const command = new GCodeCommand('G0 X1 Y2', 'g0', { x: 1, y: 2 });
-  const interpreter = new Interpreter();
-  const job = new Job();
+  test('starts a new path if the travel type changes from Extrusion to Travel', () => {
+    const command1 = new GCodeCommand('G1 X1 Y2 E3', 'g1', { x: 1, y: 2, e: 3 });
+    const command2 = new GCodeCommand('G0 X3 Y4', 'g0', { x: 3, y: 4 });
+    const interpreter = new Interpreter();
+    const job = new Job();
+    interpreter.execute([command1], job);
 
-  interpreter.g0(command, job);
+    interpreter.g0(command2, job);
 
-  expect(job.paths.length).toEqual(0);
-  expect(job.inprogressPath?.travelType).toEqual(PathType.Travel);
-});
+    expect(job.paths.length).toEqual(1);
+    expect(job.inprogressPath?.travelType).toEqual('Travel');
+  });
 
-test(".G0 assigns the extrusion type if there's extrusion", () => {
-  const command = new GCodeCommand('G1 X1 Y2 E3', 'g1', { x: 1, y: 2, e: 3 });
-  const interpreter = new Interpreter();
-  const job = new Job();
+  test('.G1 is an alias to .G0', () => {
+    const interpreter = new Interpreter();
 
-  interpreter.g0(command, job);
-
-  expect(job.paths.length).toEqual(0);
-  expect(job.inprogressPath?.travelType).toEqual('Extrusion');
-});
-
-test('.G0 assigns the travel type if the extrusion is a retraction', () => {
-  const command = new GCodeCommand('G0 E-2', 'g0', { e: -2 });
-  const interpreter = new Interpreter();
-  const job = new Job();
-
-  interpreter.g0(command, job);
-
-  expect(job.paths.length).toEqual(0);
-  expect(job.inprogressPath?.travelType).toEqual('Travel');
-});
-
-test('.G0 assigns the travel type if the extrusion is a retraction', () => {
-  const command = new GCodeCommand('G0 E-2', 'g0', { e: -2 });
-  const interpreter = new Interpreter();
-  const job = new Job();
-
-  interpreter.g0(command, job);
-
-  expect(job.paths.length).toEqual(0);
-  expect(job.inprogressPath?.travelType).toEqual('Travel');
-});
-
-test('.G0 starts a new path if the travel type changes from Travel to Extrusion', () => {
-  const command1 = new GCodeCommand('G0 X1 Y2', 'g0', { x: 1, y: 2 });
-  const command2 = new GCodeCommand('G1 X3 Y4 E5', 'g1', { x: 3, y: 4, e: 5 });
-  const interpreter = new Interpreter();
-  const job = new Job();
-  interpreter.execute([command1], job);
-
-  interpreter.g0(command2, job);
-
-  expect(job.paths.length).toEqual(1);
-  expect(job.inprogressPath?.travelType).toEqual('Extrusion');
-});
-
-test('.G0 starts a new path if the travel type changes from Extrusion to Travel', () => {
-  const command1 = new GCodeCommand('G1 X1 Y2 E3', 'g1', { x: 1, y: 2, e: 3 });
-  const command2 = new GCodeCommand('G0 X3 Y4', 'g0', { x: 3, y: 4 });
-  const interpreter = new Interpreter();
-  const job = new Job();
-  interpreter.execute([command1], job);
-
-  interpreter.g0(command2, job);
-
-  expect(job.paths.length).toEqual(1);
-  expect(job.inprogressPath?.travelType).toEqual('Travel');
-});
-
-test('.G1 is an alias to .G0', () => {
-  const interpreter = new Interpreter();
-
-  expect(interpreter.g1).toEqual(interpreter.g0);
+    expect(interpreter.g1).toEqual(interpreter.g0);
+  });
 });
 
 test('.G20 sets the units to inches', () => {
